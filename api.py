7e5858a47012a7d385e2cde6d08fd6534bb33fcb
Veerat Beri <viratberi@gmail.com>
2021-05-22 03:19:31 +0530

    wip

--- api.py
+++ api.py
@@ -1,19 +1,19 @@
 import sys
 from expression.service import ExpressionService
 from models import User
+from serializers import UserSerializer
 
 
 class ApiInterface:
     def __init__(self):
         self.service_inst = ExpressionService()
 
-    def is_user_allowed(self, user: User, validation_expression):
+    def is_user_allowed(self, user: User, expression: str):
         try:
-            self.serializer_type.validate(user_data)
-            expression_parser = self.parser_type(validation_expression, self.serializer_type())
-            expression_parser.validate()
-            valid_parsed_expression = expression_parser.get_parsed_expression()
-            return self.service_inst.any_condition_match(user_data, valid_parsed_expression)
-
+            user_context = UserSerializer(user).data
+            return self.service_inst.evaluate_postfix_expression(user_context, expression)
         except:
             print("Error: ", sys.exc_info()[0])
+
+    def create_user(self):
+        pass
